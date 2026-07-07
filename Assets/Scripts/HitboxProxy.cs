edd38c86a2ecbbcf2ce5fc63987a787bae8bf7e9
Megomago <samsungindonesia5671@gmail.com>
2026-07-07 21:40:08 +0700

    feat: tambah HitboxProxy + VehicleHitboxInitializer buat cache komponen collision

---
+++ Assets/Scripts/HitboxProxy.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HitboxProxy : MonoBehaviour
+{
+    public VehicleModuleComponent moduleComponent;
+    public WheelHealth wheelHealth;
+    public VehicleCriticalPart criticalPart;
+    public SimpleTarget simpleTarget;
+    public VehicleStatsManager statsManager;
+}
---
+++ Assets/Scripts/VehicleHitboxInitializer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class VehicleHitboxInitializer : MonoBehaviour
+{
+    private void Awake()
+    {
+        VehicleStatsManager vsm = GetComponent<VehicleStatsManager>();
+        if (vsm == null) return;
+
+        Collider[] colliders = GetComponentsInChildren<Collider>(true);
+        foreach (var col in colliders)
+        {
+            if (col.isTrigger) continue;
+
+            GameObject go = col.gameObject;
+            HitboxProxy proxy = go.GetComponent<HitboxProxy>();
+            if (proxy == null)
+                proxy = go.AddComponent<HitboxProxy>();
+
+            proxy.moduleComponent = go.GetComponentInParent<VehicleModuleComponent>();
+            proxy.wheelHealth = go.GetComponentInParent<WheelHealth>();
+            proxy.criticalPart = go.GetComponentInParent<VehicleCriticalPart>();
+            proxy.simpleTarget = go.GetComponentInParent<SimpleTarget>();
+            proxy.statsManager = vsm;
+        }
+    }
+}
